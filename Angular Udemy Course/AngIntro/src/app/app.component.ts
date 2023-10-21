e2935a085e711677a148cc94644b09b42c006a9b
makaranddhamale <makarand.dhamale09@gmail.com>
2023-10-21 18:53:27 +0530

    Data binding in Angular

--- Angular Udemy Course/AngIntro/src/app/app.component.ts
+++ Angular Udemy Course/AngIntro/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  ViewChild,
+} from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'AngIntro';
+  parentMessage: string = 'Message Coming from Parent component';
+  message!: string;
+  fromChildOutput!: string;
+
+  @ViewChild(PostComponent) childComp!: PostComponent;
+
+  constructor() {
+    console.log(this.childComp);
+  }
+  ngAfterViewInit(): void {
+    console.log(this.childComp);
+    this.message = this.childComp.childMessage;
+  }
+
+  recieveMessage($event: string) {
+    this.fromChildOutput = $event;
+  }
 }
--- Angular Udemy Course/AngIntro/src/app/post/post.component.ts
+++ Angular Udemy Course/AngIntro/src/app/post/post.component.ts
@@ -1,8 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {}
+export class PostComponent implements OnInit {
+  title: string = 'List of Post';
+  messagePost: string = 'Message Post';
+
+  postParentMessage: string = 'Message coming from post parent';
+
+  childMessage: string = 'From Child Component';
+
+  outputChildMessage = 'Message from child component via output';
+
+  @Output() messageEvent = new EventEmitter<string>();
+
+  @Input() fromParent!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  sendMessage() {
+    this.messageEvent.emit(this.outputChildMessage);
+  }
+}
