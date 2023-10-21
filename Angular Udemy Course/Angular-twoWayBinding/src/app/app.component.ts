42949aed00ce0fd89cccb00baebc52b6ed965dad
makaranddhamale <makarand.dhamale09@gmail.com>
2023-10-21 22:38:05 +0530

    Property, class, event binding completed

--- Angular Udemy Course/Angular-twoWayBinding/src/app/app.component.ts
+++ Angular Udemy Course/Angular-twoWayBinding/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   message: string = 'Message from typescript component file';
+  imageUrl: string =
+    'https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+  //This way we can dynamically set the color
+  bool: boolean = true;
+
+  buttonClick() {
+    console.log('Button Clicked');
+  }
+
+  onKeyup() {
+    console.log('Enter key pressed');
+  }
 }
