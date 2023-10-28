81f99a9c5cf29c706b2ad5971ae59d5f37e8bb40
makaranddhamale <makarand.dhamale09@gmail.com>
2023-10-28 16:08:04 +0530

    Validation Added

--- Angular Udemy Course/Angular-services/src/app/app.component.ts
+++ Angular Udemy Course/Angular-services/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-services';
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+  }
+
+  getValue(f: NgModel) {
+    console.log(f);
+  }
 }
