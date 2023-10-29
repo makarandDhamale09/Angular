1fdfde6dfd729157d59dcf14c8c933251f79b59a
makaranddhamale <makarand.dhamale09@gmail.com>
2023-10-29 13:13:17 +0530

    Reactive forms completed

--- Angular Udemy Course/Angular-services/src/app/app.component.ts
+++ Angular Udemy Course/Angular-services/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  NgModel,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +15,60 @@ import { FormControl, NgForm, NgModel } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular-services';
+  form: FormGroup;
 
+  constructor(fb: FormBuilder) {
+    fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(5)]],
+    });
+
+    this.form = new FormGroup({
+      fullName: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'),
+      ]),
+      address: new FormControl(null, Validators.required),
+      skills: new FormArray([]),
+    });
+  }
+
+  get fullname() {
+    return this.form.get('fullName');
+  }
+
+  addSkills(skill: HTMLInputElement) {
+    this.Skills.push(new FormControl(skill.value));
+    skill.value = '';
+  }
+
+  get Skills() {
+    return this.form.get('skills') as FormArray;
+  }
+
+  removeSkills(index: number) {
+    this.Skills.removeAt(index);
+  }
+
+  onReactiveSubmit() {
+    console.log(this.form.value);
+  }
+
+  getValueReactive() {
+    console.log('Reactive Form Change : ', this.form);
+  }
+
+  onSubmitReactive() {
+    console.log(this.form.value);
+  }
+
+  // This is used for Template driven form control
   onSubmit(f: NgForm) {
-    console.log(f);
+    console.log(f.value);
   }
 
   getValue(f: NgModel) {
--- Angular Udemy Course/Angular-services/src/app/app.module.ts
+++ Angular Udemy Course/Angular-services/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostService } from './Services/post.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PostComponent, PostListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [PostService],
   bootstrap: [AppComponent],
 })
