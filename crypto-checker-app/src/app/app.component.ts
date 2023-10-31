2dfaf1bdbc9ea464b1d777d2aeb20a26cc7acc67
makaranddhamale <makarand.dhamale09@gmail.com>
2023-10-31 18:50:19 +0530

    Added toolbar

--- crypto-checker-app/src/app/app.component.ts
+++ crypto-checker-app/src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedCurrency: string = 'INR';
+
+  constructor() {}
+
+  sendCurrency(event: string) {
+    console.log(event);
+  }
+}
