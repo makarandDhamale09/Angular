7a1c67df3016aa27a5d8ddb353a3938a6534840d
makaranddhamale <makarand.dhamale09@gmail.com>
2023-11-03 17:26:09 +0530

    Code Change

--- crypto-checker-app/src/app/coin-list/coin-list.component.ts
+++ crypto-checker-app/src/app/coin-list/coin-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
-  styleUrls: ['./coin-list.component.css']
+  styleUrls: ['./coin-list.component.css'],
 })
-export class CoinListComponent {
+export class CoinListComponent implements OnInit {
+  bannerData: any = [];
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getBannerData();
+    this.getAllData();
+  }
+
+  getBannerData() {
+    this.api.getTrendingCurrency('INR').subscribe((res) => {
+      console.log(res);
+      this.bannerData = res;
+    });
+  }
+
+  getAllData() {
+    this.api.getCurrency('INR').subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
